83cb44f7e9f1ed657fe8601eb57ccbc21b787d19
Shivam <rajmishra112001@gmail.com>
2025-03-10 20:34:46 +0530

    1
    last

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Shivam Tiwari</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />
+            I develop Native App's, Machine learning projects, user <br className="sm:block hidden" />
             interfaces and web applications in MERN.
           </p>
         </div>
